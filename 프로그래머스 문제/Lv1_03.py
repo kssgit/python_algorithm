8c41225d416aae7011c8292dbc885256cba04305
kssgit <76714467+kssgit@users.noreply.github.com>
2021-06-14 00:45:39 +0900

    README ADD

--- 프로그래머스 문제/Lv1_03.py
+++ 프로그래머스 문제/Lv1_03.py
@@ -4,6 +4,7 @@ def solution(participant, completion):
     completion.sort()
     for par , comp in zip(participant, completion):
         if par !=comp:
+            #동명이인 값 
             return par
     return participant.pop()
 
@@ -21,3 +22,18 @@ import collections
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
+
+
+## 다른 사람 풀이 (해시 사용)
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
