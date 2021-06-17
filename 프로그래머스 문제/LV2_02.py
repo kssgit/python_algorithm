9a2f32e8415975e8b8c27767f293ca702de5fb21
kssgit <76714467+kssgit@users.noreply.github.com>
2021-06-17 15:36:51 +0900

    Create LV2_02.py

---
+++ 프로그래머스 문제/LV2_02.py
@@ -0,0 +1,29 @@
+# 오픈 채팅방
+
+def solution(record):
+    answer = []
+    dic = {}
+    id = []
+    for r in record:
+        a = r.split(' ')
+        if a[0] == 'Enter':
+            dic[a[1]] = a[2]
+            id.append([a[1],'님이 들어왔습니다.'])
+        elif a[0] == 'Leave':
+            id.append([a[1],'님이 나갔습니다.'])
+        else:
+            dic[a[1]] = a[2]
+    print(dic)
+    print(id)
+    
+    for i in id:
+        answer.append(dic[i[0]]+i[1])
+
+    return answer
+
+
+
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+
+result=solution(record)
+print(result)
