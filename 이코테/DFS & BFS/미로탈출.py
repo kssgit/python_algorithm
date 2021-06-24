ae17668aec98080c7b8a3d3746277bcd459e1ccf
kssgit <76714467+kssgit@users.noreply.github.com>
2021-06-24 14:09:13 +0900

    Create 미로탈출(BFS).py

---
+++ 이코테/DFS & BFS/미로탈출.py
@@ -0,0 +1,38 @@
+from collections import deque
+n ,m = 5,6
+
+graph = [
+    [1,0,1,0,1,0],
+    [1,1,1,1,1,1],
+    [0,0,0,0,0,1],
+    [1,1,1,1,1,1],
+    [1,1,1,1,1,1]
+]
+
+def solution(graph,n,m):
+    queue = deque()
+    queue.append((0,0))
+    # 상하 좌우
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+
+
+    while queue:
+        x,y = queue.popleft()
+        # 상하 좌우 모두 탐색
+        for i in range(4):
+            nx = x +dx[i]
+            ny = y +dy[i]
+            ##nx,ny 값이 graph의 index값을 넘어가면 continue
+            if nx <0 or nx >= n  or ny <0 or ny >= m:
+                continue
+            ## 해당 좌표가 0이면 continue
+            if not graph[nx][ny]:
+                continue
+            ## 해당 그래프의 값을 이전 방문의 graph 값의 +1을 해서 count를 센다
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] +1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(solution(graph,n,m))
--- 이코테/DFS & BFS/음료수 얼려 먹기.py
+++ 이코테/DFS & BFS/음료수 얼려 먹기.py
@@ -5,7 +5,7 @@ graph = []
 for i in range(n):
     graph.append(list(map(int,input())))
 
-
+## 해당 그래프와 인접한 0인 모든 그래프 방문 처리
 def dfs(y,x):
     if y <= -1 or y >= n or x <= -1 or x>=m:
         return False
