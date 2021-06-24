dd076127022f4b2e9b117dbeae4ecd32b0b38636
kssgit <76714467+kssgit@users.noreply.github.com>
2021-06-25 01:47:14 +0900

    Create LV2_05.py

---
+++ 프로그래머스 문제/LV2/LV2_05.py
@@ -0,0 +1,33 @@
+# 타겟 넘버
+answer = 0
+def solution(numbers, target):
+    global answer
+    dfs(0,numbers,target,0)
+    return answer
+
+def dfs(indx , numbers, target, value):
+    global answer 
+    if indx == len(numbers) and target == value:
+        answer +=1
+        return 
+    if indx == len(numbers):
+        return 
+    dfs(indx+1,numbers,target,value+numbers[indx])
+    dfs(indx+1,numbers,target,value-numbers[indx])
+
+
+numbers =[1, 1, 1, 1, 1]
+target = 3
+
+
+# 다른 사람 풀이
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+
+print(solution(numbers,target))
